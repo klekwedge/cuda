640e7b7a45d3a6d55c0d9c789d11ce12d894bcf8
klekkwedge <antonov.andrey.10101@gmail.com>
2022-04-28 14:02:18 +0300

    feat: add animation on scroll

--- cuda/js/script.js
+++ cuda/js/script.js
@@ -2,9 +2,11 @@
 
 import burger from "./modules/burger";
 import circles from "./modules/circles";
+import animationScroll from "./modules/anim-scroll";
 
 window.addEventListener("DOMContentLoaded", () => {
   burger();
+  animationScroll();
   circles();
 });
 
